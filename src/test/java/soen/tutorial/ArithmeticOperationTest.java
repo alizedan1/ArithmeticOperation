295bfa934a808ac37cbc6096c519f6b05ffbc892
alizedan1 <71774788+alizedan1@users.noreply.github.com>
2025-04-06 11:34:47 -0400

    Update ArithmeticOperationTest.java

--- src/test/java/soen/tutorial/ArithmeticOperationTest.java
+++ src/test/java/soen/tutorial/ArithmeticOperationTest.java
@@ -14,21 +14,22 @@ public class ArithmeticOperationTest {
 		ArithmeticOperation operations = new ArithmeticOperation();
 		Integer actual = operations.addOrSub(2, 6);
 		Integer expected = 8;
-		assertEquals(expected, actual);
+		//assertEquals(expected, actual);
+		fail("must fail");
 
 	}
-//
-  //  @Test
-   // public void testSub()
-    //{
 
-      //  ArithmeticOperation operations = new ArithmeticOperation();
-       // Integer actual = operations.addOrSub(6, 2);
-       // Integer expected = 4;
-        //assertEquals(expected, actual);
+  /*  @Test
+    public void testSub()
+    {
 
-    //}
+        ArithmeticOperation operations = new ArithmeticOperation();
+        Integer actual = operations.addOrSub(6, 2);
+        Integer expected = 4;
+        assertEquals(expected, actual);
 
-	//
+    } **/
+
+	
 
 }
